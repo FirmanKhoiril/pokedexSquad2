3e187d26c6543709c4d3c37b577060739fabaae4
FirmanKhoiril <firmankhoiril13@gmail.com>
2024-11-17 17:28:30 +0700

    change naming function and nav-links

---
+++ pokedex/src/pages/PokemonTradingCardGame.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PokemonTradingCardGame = () => {
+  return (
+    <section className='container mx-auto'>PokemonTradingCardGame</section>
+  )
+}
+
+export default PokemonTradingCardGame
