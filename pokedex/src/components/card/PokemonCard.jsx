96c675abf798b7691ac66ca2efa9f54f5b1c230f
FirmanKhoiril <firmankhoiril13@gmail.com>
2024-11-17 11:11:20 +0700

    removing some unused code, make button bookmark work independent because later it will add the pokemon in the server (works like favorite pokemon)

--- pokedex/src/components/card/PokemonCard.jsx
+++ pokedex/src/components/card/PokemonCard.jsx
@@ -2,33 +2,51 @@ import { typeColors } from "../../services/DataDummy";
 import { Link } from "react-router-dom";
 
 const PokemonCard = ({ image, name, id, types }) => {
+  const handleBookmarkClick = (e) => {
+    e.stopPropagation();
+    console.log("test");
+  };
+
   return (
-    <Link
-      to={`/pokemon/${id}`}
+    <div
       className="border-2 border-blue-500 rounded-lg shadow-md p-4 w-full sm:w-[300px] lg:w-72 flex flex-col items-center relative cursor-pointer transition-transform duration-300 transform hover:rotate-1 hover:-translate-y-2"
     >
-      <img src={image || "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"} alt={name} loading="lazy" className="w-32 h-32 mb-4" />
+      <Link
+        to={`/pokemon/${id}`}
+        className="flex flex-col items-center w-full"
+      >
+        <img
+          src={image || "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"}
+          alt={name}
+          loading="lazy"
+          className="w-32 h-32 mb-4"
+        />
 
-      <h2 className="text-lg font-bold text-gray-800 capitalize">{name}</h2>
-      <p className="text-gray-500 text-sm">#{String(id).padStart(4, '0')}</p>
+        <h2 className="text-lg font-bold text-gray-800 capitalize">{name}</h2>
+        <p className="text-gray-500 text-sm">#{String(id).padStart(4, '0')}</p>
 
-      <div className="flex space-x-2 mt-4">
-        {types?.map((type) => {
-          const typeName = type.type.name;
-          const bgColor = typeColors[typeName] || typeColors.Default;
+        <div className="flex space-x-2 mt-4">
+          {types?.map((type) => {
+            const typeName = type.type.name;
+            const bgColor = typeColors[typeName] || typeColors.Default;
 
-          return (
-            <span
-              key={typeName}
-              className={`px-3 py-1 capitalize rounded-full text-sm text-white ${bgColor}`}
-            >
-              {typeName}
-            </span>
-          );
-        })}
-      </div>
+            return (
+              <span
+                key={typeName}
+                className={`px-3 py-1 capitalize rounded-full text-sm text-white ${bgColor}`}
+              >
+                {typeName}
+              </span>
+            );
+          })}
+        </div>
+      </Link>
 
-      <div className="absolute top-2 right-2">
+      <button
+        type="button"
+        onClick={handleBookmarkClick} // Bookmark button click
+        className="absolute z-20 p-2 top-2 right-2"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -43,8 +61,8 @@ const PokemonCard = ({ image, name, id, types }) => {
             d="M17.25 6.75v14.25L12 16.5l-5.25 4.5V6.75c0-.621.504-1.125 1.125-1.125h10.25c.621 0 1.125.504 1.125 1.125z"
           />
         </svg>
-      </div>
-    </Link>
+      </button>
+    </div>
   );
 };
 
