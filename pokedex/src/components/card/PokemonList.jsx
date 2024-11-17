1eb266afa0e2780c9753248003c4ddb9601194a2
Adnan Yazid Ardiansyah <adnanyazidar48@gmail.com>
2024-11-17 07:23:34 +0700

    Mengerjakan Fitur filter berdasarkan sort by number, name dan type

--- pokedex/src/components/card/PokemonList.jsx
+++ pokedex/src/components/card/PokemonList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PokemonCard from "./PokemonCard";
-import Filter from "../components/Filter";
+import Filter from "./Filter";
 
 const PokemonList = () => {
   const pokemonData = [
--- pokedex/src/pages/Home.jsx
+++ pokedex/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PokemonList from "../components/PokemonList";
+import PokemonList from "../components/card/PokemonList";
 
 const Home = () => {
   return (
